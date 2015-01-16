5d895ffd666c9bc1c3a67d00946dbfcfd37081b7
Jokki Nielson <yurastein@gmail.com>
2015-01-16 15:49:09 +0200

    First Commit
    This is first commit

---
+++ UpdateChecker/UpdateChecker/Classes/LinkAlreadyExist.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdateChecker.Classes
+{
+    class LinkAlreadyExist : Exception
+    {
+    }
+}
---
+++ UpdateChecker/UpdateChecker/Classes/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdateChecker.Classes
+{
+    public static class Utils
+    {
+        public static string GetFileName(string fullPath)
+        {
+            return fullPath.Substring(fullPath.LastIndexOf('\\') + 1);
+        }
+
+        public static string GetPathWithoutFileName(string fullPath)
+        {
+            string fileName = fullPath.Substring(fullPath.LastIndexOf('\\') + 1);
+            return fullPath.Substring(0, fullPath.Length - fileName.Length);
+        }
+    }
+}
